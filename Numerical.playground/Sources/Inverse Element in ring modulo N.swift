faf67c7513ec9dcb4b4b74c41a77781d43514f13
Ruziev <jjruziev@gmail.com>
2017-06-11 12:52:18 +0300

    Replaced if block with eternal operator in Inverse Element

--- Numerical.playground/Sources/Inverse Element in ring modulo N.swift
+++ Numerical.playground/Sources/Inverse Element in ring modulo N.swift
@@ -3,10 +3,5 @@ public func inverse(for n: Int, modulo m: Int) -> Int? {
     let tmp = gcdex(a: n, b: m)
     gcd = tmp.0
     x = tmp.1
-    if gcd != 1 {
-        return nil
-    } else {
-        x = (x % m + m) % m
-        return x
-    }
+    return gcd == 1 ? (x % m + m) % m : nil
 }
