95586e973f215b9a20467ceb3b26e6f6a09ab924
Ruziev <jjruziev@gmail.com>
2017-06-11 12:54:30 +0300

    Revert "Replaced if block with eternal operator in Inverse Element"
    This reverts commit faf67c7513ec9dcb4b4b74c41a77781d43514f13.

--- Numerical.playground/Sources/Inverse Element in ring modulo N.swift
+++ Numerical.playground/Sources/Inverse Element in ring modulo N.swift
@@ -3,5 +3,10 @@ public func inverse(for n: Int, modulo m: Int) -> Int? {
     let tmp = gcdex(a: n, b: m)
     gcd = tmp.0
     x = tmp.1
-    return gcd == 1 ? (x % m + m) % m : nil
+    if gcd != 1 {
+        return nil
+    } else {
+        x = (x % m + m) % m
+        return x
+    }
 }
